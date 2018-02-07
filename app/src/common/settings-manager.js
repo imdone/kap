30420dafad17f61820caa78db247f73c995d42f5
Jesse Piascik <jesse@piascik.net>
2018-02-07 10:39:38 -0700

    Update settings-manager.js

--- app/src/common/settings-manager.js
+++ app/src/common/settings-manager.js
@@ -50,8 +50,6 @@ async function init() {
   // We need to fetch a input device because if the user opens the app for the first time
   // and toggle the mic in the main window to record audio, we will not record any audio
   // if we do not have a input id stored.
-  // TODO: if no input device is available (could happen in an iMac, for example), we need id:1 gh:2 ic:gh
-  // to tell the user
   const devices = await aperture.audioDevices();
   if (devices.length > 0) {
     settings.setSync('audioInputDeviceId', devices[0].id);
